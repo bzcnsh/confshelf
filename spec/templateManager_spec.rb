de99b66a1a78f6546fbbe1518a68ac9dcd0b7cff
Bill Zheng <bzcnsh@yahoo.com>
2015-05-15 20:17:52 -0700

    refactor templateManager, complete its spec

--- spec/templateManager_spec.rb
+++ spec/templateManager_spec.rb
@@ -3,22 +3,115 @@ require  "templateManager"
 
 describe "TemplateManager test/" do
   before do
-    @device_rt1 = double(:name => "rt1", :OS => "IOS", :OSVersion=>"12.0")
-    @device_rt3 = double(:name => "rt3", :OS => "IOS", :OSVersion=>"12.6")
-    templateTree = YAML.load_file('data/template.yml')
+    @device_rt110 = double(:name => "rt110", :OS => "IOS", :OSVersion=>"11.00")
+    @device_rt120 = double(:name => "rt120", :OS => "IOS", :OSVersion=>"12.00")
+    @device_rt121 = double(:name => "rt121", :OS => "IOS", :OSVersion=>"12.01")
+    @device_rt130 = double(:name => "rt130", :OS => "IOS", :OSVersion=>"13.00")
+    @device_rt131 = double(:name => "rt131", :OS => "IOS", :OSVersion=>"13.01")
+    @device_rt133 = double(:name => "rt133", :OS => "IOS", :OSVersion=>"13.03")
+    @device_rt138 = double(:name => "rt138", :OS => "IOS", :OSVersion=>"13.08")
+    @device_rt139 = double(:name => "rt139", :OS => "IOS", :OSVersion=>"13.09")
+    @device_rt140 = double(:name => "rt140", :OS => "IOS", :OSVersion=>"14.00")
+    templateTree = YAML.load_file('test-data/template.yml')
     @tm = TemplateManager.new(templateTree)
   end
-  
+  it "formatVersionConstraint >12.3"  do
+    expect(@tm.formatVersionConstraint(">12.3")).to eql(">\"12.3\"")
+  end
+  it "formatVersionConstraint =12.3"  do
+    expect(@tm.formatVersionConstraint("=12.3")).to eql("==\"12.3\"")
+  end
+  it "formatVersionConstraint <=12.3"  do
+    expect(@tm.formatVersionConstraint("<=12.3")).to eql("<=\"12.3\"")
+  end
+
+  it "matchAVersionContraint(12.0<=12.0)" do
+    expect(@tm.matchAVersionContraint("12.0", "<=\"12.0\"")).to be true
+  end
+  it "matchAVersionContraint(12.0<12.1)" do
+    expect(@tm.matchAVersionContraint("12.0", "<\"12.1\"")).to be true
+  end
+  it "matchAVersionContraint(12.0>12.0)" do
+    expect(@tm.matchAVersionContraint("12.0", ">\"12.0\"")).to be false
+  end
+  it "device template for device IOS 11.0 does not exist" do
+    expect(@tm.lookupTemplateFilename(@device_rt110, "device")).to be_nil
+  end
+
   it "device template for device IOS 12.0 is device_ios_12.erb" do
-    expect(@tm.lookupTemplateFilename(@device_rt1, "device")).to eq("device_ios_12.erb")
+    expect(@tm.lookupTemplateFilename(@device_rt120, "device")).to eq("device_ios_12.erb")
   end
   it "vlan template for device IOS 12.0 is vlan_ios_12.erb" do
-    expect(@tm.lookupTemplateFilename(@device_rt1, "vlan")).to eq("vlan_ios_12.erb")
+    expect(@tm.lookupTemplateFilename(@device_rt120, "vlan")).to eq("vlan_ios_12.erb")
   end
-  it "device template for device IOS 12.6 is device_ios_12.erb" do
-    expect(@tm.lookupTemplateFilename(@device_rt3, "device")).to eq("device_ios_12.erb")
+
+  it "device template for device IOS 12.1 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt121, "device")).to eq("device_ios_12.erb")
   end
-  it "vlan template for device IOS 12.6 is vlan_ios_12.erb" do
-    expect(@tm.lookupTemplateFilename(@device_rt3, "vlan")).to eq("vlan_ios_12.erb")
+  it "vlan template for device IOS 12.1 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt121, "vlan")).to eq("vlan_ios_12.erb")
+  end
+
+  it "device template for device IOS 13.0 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt130, "device")).to eq("device_ios_12.erb")
+  end
+  it "vlan template for device IOS 13.0 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt130, "vlan")).to eq("vlan_ios_12.erb")
+  end
+  it "hsrp template for device IOS 13.0 is hsrp_ios_130.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt130, "hsrp")).to eq("hsrp_ios_130.erb")
+  end
+
+  it "aabb template for aabb IOS 13.1 does not exist" do
+    expect(@tm.lookupTemplateFilename(@device_rt131, "aabb")).to be_nil
+  end
+  it "device template for device IOS 13.1 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt131, "device")).to eq("device_ios_12.erb")
+  end
+  it "vlan template for device IOS 13.1 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt131, "vlan")).to eq("vlan_ios_12.erb")
+  end
+  it "hsrp template for device IOS 13.1 is hsrp_ios_130.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt131, "hsrp")).to eq("hsrp_ios_130.erb")
+  end
+
+  it "device template for device IOS 13.3 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt133, "device")).to eq("device_ios_12.erb")
+  end
+  it "vlan template for device IOS 13.3 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt133, "vlan")).to eq("vlan_ios_12.erb")
+  end
+  it "grrp template for device IOS 13.3 is grrp_ios_130.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt133, "grrp")).to eq("grrp_ios_13.erb")
+  end
+  
+  it "device template for device IOS 13.8 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt138, "device")).to eq("device_ios_12.erb")
+  end
+  it "vlan template for device IOS 13.8 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt138, "vlan")).to eq("vlan_ios_12.erb")
+  end
+  it "grrp template for device IOS 13.8 is grrp_ios_130.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt138, "grrp")).to eq("grrp_ios_13.erb")
+  end
+
+  it "device template for device IOS 13.9 is device_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt139, "device")).to eq("device_ios_12.erb")
+  end
+  it "vlan template for device IOS 13.9 is vlan_ios_12.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt139, "vlan")).to eq("vlan_ios_12.erb")
+  end
+  it "grrp template for device IOS 13.9 is grrp_ios_13.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt139, "grrp")).to eq("grrp_ios_13.erb")
+  end
+  it "hsrp template for device IOS 13.9 is hsrp_ios_13.8.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt139, "hsrp")).to eq("hsrp_ios_13.8.erb")
+  end
+
+  it "device template for device IOS 14 is null" do
+    expect(@tm.lookupTemplateFilename(@device_rt140, "device")).to be_nil
+  end
+  it "hsrp template for device IOS 14 is hsrp_ios_14.erb" do
+    expect(@tm.lookupTemplateFilename(@device_rt140, "hsrp")).to eq("hsrp_ios_14.erb")
   end
 end
--- templateManager.rb
+++ templateManager.rb
@@ -1,52 +1,93 @@
+#manages template, look up for template, given device and component
 class TemplateManager
   def initialize(templateArray)
     @templateHash = {}
     @templateArray = templateArray
-    templateArray.each { |t| 
+    templateArray.each { |t|
+      t["versionConstraints"].map!{|x| x = formatVersionConstraint(x)}
       @templateHash[t['name']] = t
     }
   end
-def lookupTemplateFilename(device, component)
-  #match by device OS, OS version
-  #check component template
-  #if component template's not defined, lookup the template ancestor tree
-  templateFile = lookupTemplate2(device.OS, device.OSVersion, component.downcase)
-end
-def lookupTemplate(device, component)
-  #match by device OS, OS version
-  #check component template
-  #if component template's not defined, lookup the template ancestor tree
-  template = lookupTemplateFilename(device, component)
-  #caching for performance improvement
-  return ERB.new File.new("template/#{template}").read, nil, "<>"
-end
+  def formatVersionConstraint(input)
+    if input =~ /^([<=>]+)(.+)$/
+      op = $1
+      version = $2
+      #replace "=" with "=="
+      op.sub!(/^=$/, "==")
+      #add quotes to version number
+      if not version =~ /^['"]/
+        version = "\"#{version}\""
+      end
+      return "#{op}#{version}"
+    else
+      raise "bad versionContraint format: #{input}"
+    end
+  end
+  #a device's version number is compared against every versionConstraint
+  #when all constraints are satisfied, the template is a match for the device
+  def matchAVersionContraint(left, right)
+    if not left =~ /^['"]/
+      left = "\"#{left}\""
+    end
+    eval "#{left}#{right}"
+  end
+  def lookupTemplateFilename(device, component)
+    templateFile = lookupTemplate2(device.OS, device.OSVersion, component.downcase)
+  end
+  def lookupTemplate(device, component)
+    #match by device OS, OS version
+    #check component template
+    #if component template's not defined, lookup the template ancestor tree
+    template = lookupTemplateFilename(device, component)
+    #caching for performance improvement
+    if template.nil?
+      return ERB.new File.new("template/#{template}").read, nil, "<>"
+    else
+      return nil
+    end
+  end
   def lookupTemplate2(os, osversion, component)
-    template = @templateArray.select{|x| x['OS']==os and x['OSVersion']==osversion}
-    if template.length==0
+    t2=[]
+    template = @templateArray.select{|x| x['OS']==os}
+    template.each {|x|
+      isMatch=true
+      x['versionConstraints'].each {|y| 
+        #every constraint has to match
+        if not matchAVersionContraint(osversion, y)
+          isMatch=false
+          next
+        end
+      }
+      if(isMatch)
+        t2<<x
+      end
+    }
+    if t2.length==0
       puts "no template for #{os}, #{osversion}"
-      raise "no template for #{os}, #{osversion}"
+      return nil
     end
-    if template.length>1
+    if t2.length>1
       puts "more than one template for #{os}, #{osversion}"
-      raise "more than one template for #{os}, #{osversion}"
+      return nil
     end
-    t = lookupTemplate3(template[0]["name"], component)
+    t = lookupTemplate3(t2[0]["name"], component)
     if t.nil?
       puts "no template for #{os}, #{osversion}. #{component}"
-      raise "no template for #{os}, #{osversion}. #{component}"
+      return nil
     end
-    return t      
+    return t
   end
   #look up the ancestor tree
   def lookupTemplate3(templateName, component)
     if @templateHash[templateName].has_key?("#{component}_template")
       return @templateHash[templateName]["#{component}_template"]
     else
-      if @templateHash[templateName].has_key?("super_template")
+      if @templateHash[templateName].has_key?("super_template") and @templateHash.has_key?(@templateHash[templateName]["super_template"])
         return lookupTemplate3(@templateHash[templateName]["super_template"], component)
       else
         return nil
       end
     end
   end
+
 end
